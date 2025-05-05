4f0024677a2083e098cdb8a608b53a0438eec844
TR014777 <pauloflaflaflafla@gmail.com>
2025-05-05 09:56:21 -0300

    Adicionei unidades para produtos

--- prototipo.py
+++ prototipo.py
@@ -1,11 +1,14 @@
 produtos = []
 precos = []
+quantidades = []
 ativo = True
 while(ativo == True):
     produto = input("Digite qual é o produto(exemplo: Miojo, Pasta de Dente):\n")
     preco = float(input("Digite o preço do produto(exemplo: 3, 5.90, 12.34):\n"))
+    quantidade = int(input("Digite quantas unidades:\n"))
     produtos.append(produto)
-    precos.append(preco)
+    precos.append(preco * quantidade)
+    quantidades.append(quantidade)
     continuar = input("Deseja continuar? \nDigite 's' se sim. caso não, digite qualquer tecla.\n")
     match continuar:
         case 's':
@@ -13,8 +16,8 @@ while(ativo == True):
         case _:
             ativo = False
 
-for produto, preco in zip(produtos, precos):
-    print(f"{produto}: R$ {preco:.2f}")
+for produto, quantidade, preco in zip(produtos, quantidades, precos):
+    print(f"{produto} [{quantidade} unidade(s)]: R$ {preco:.2f}")
 total = 0
 for preco in precos:
     total += preco
@@ -26,10 +29,10 @@ print("Digite o número para selecionar a lista ordenada abaixo:")
 print("1. Adicionar cupom de desconto")
 print("2. Remover item da lista")
 print("3. Cancelar a compra\n")
-respota = input("Ou Digite qualquer tecla para finalizar a compra.\n")
+resposta = input("Ou Digite qualquer tecla para finalizar a compra.\n")
 
 
-match respota:
+match resposta:
     case 1:
         pass
         ## basicamente vai adicionar um cupom e ganhar um desconto
