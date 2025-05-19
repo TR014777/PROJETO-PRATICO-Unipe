82e4612d8968868c6584086f2978181efb86c630
Andreas <andrewandrei.acc@gmail.com>
2025-05-19 14:13:57 -0300

    Métodos de pagamento (Crédito e Débito)

--- prototipo.py
+++ prototipo.py
@@ -1,47 +1,137 @@
 produtos = []
 precos = []
 quantidades = []
-ativo = True
-while(ativo == True):
-    produto = input("Digite qual é o produto(exemplo: Miojo, Pasta de Dente):\n")
-    preco = float(input("Digite o preço do produto(exemplo: 3, 5.90, 12.34):\n"))
-    quantidade = int(input("Digite quantas unidades:\n"))
-    produtos.append(produto)
-    precos.append(preco * quantidade)
-    quantidades.append(quantidade)
-    continuar = input("Deseja continuar? \nDigite 's' se sim. caso não, digite qualquer tecla.\n")
-    match continuar:
-        case 's':
+cupons_disponiveis = {
+    "DESC5": 5.00,
+    "DESC10": 10.00,  # Desconto de R$ 10,00
+    "DESC20": 20.00,  # Desconto de R$ 20,00
+    "DESC30": 30.00,  # Desconto de R$ 30,00
+}
+cupons_ativados = []
+
+def registrar_mercadorias():
+    ativo = True
+    while(ativo == True):
+        produto = input("Digite qual é o produto(exemplo: Miojo, Pasta de Dente):\n")
+        preco = float(input("Digite o preço do produto(exemplo: 3, 5.90, 12.34):\n"))
+        quantidade = int(input("Digite quantas unidades:\n"))
+        produtos.append(produto)
+        precos.append(preco * quantidade)
+        quantidades.append(quantidade)
+        continuar = input("Deseja continuar? \nDigite 's' se sim. caso não, digite qualquer tecla.\n")
+        match continuar:
+            case 's':
+                pass
+            case _:
+                ativo = False
+                mostrar_lista()
+
+def calcular_desconto():
+    desconto_total = 0
+    for cupom in cupons_ativados:
+        desconto_total += cupons_disponiveis[cupom]
+    return desconto_total
+
+def metodo_de_pagamento():
+    print("\nQual metodo de pagamento deseja utilizar?")
+    print("1. Cartão Débito ou Crédito.")
+    print("2. Pagamento por Pix.")
+    print("3. Pagamento com dinheiro.\n")
+    resposta = input("\n")
+
+    match resposta:
+        case '1':
+            print("\nDeseja utilizar Débito ou Crédito?")
+            print("1. Débito.")
+            print("2. Crédito.\n")
+            cartao = input("\n")
+
+            match cartao:
+                case '1' | '2':
+                    print("\nNos informe o número de seu cartão. Não deve conter os espaçamentos entre os números.\n")
+                    while True:
+                            try:
+                                numero_cartao = int(input())
+                                if 0000000000000000 < numero_cartao < 9999999999999999:
+                                    print("\nPor favor, digite a data de vencimento do seu cartão.\n")
+                                    mes = int(input("Mês: "))
+                                    ano = int(input("Ano: "))
+                                    if 0 < mes < 13:
+                                        pass
+                                    if ano <= 2025:
+                                        pass
+                                    cvv = int(input("\nAgora, digite o seu CVV.\n"))
+                                    if 100 < cvv < 999:
+                                        pass
+                                    conclusao = input("\nSuas credenciais estão corretas, deseja concluir a compra? Digite s para sim e n para não.\n")
+                                    match conclusao:
+                                        case 's':
+                                            print("\nSua compra foi concluída! Muito obrigado por comprar conosco!")
+                                            break
+                                        case 'n':
+                                            break ## ainda não sei o que pôr aqui.
+                                    if ano == 2025 and mes < 6:
+                                        print("\nO seu cartão está vencido.")
+                                        break
+                                print("\nO número do cartão é inválido.")
+                            except ValueError:
+                                print("\nNão foi digitado um número.")
+
+def mostrar_lista():
+    for produto, quantidade, preco in zip(produtos, quantidades, precos):
+        print(f"{produto} [{quantidade} unidade(s)]: R$ {preco:.2f}")
+    total = 0
+    for preco in precos:
+        total += preco
+    desconto = calcular_desconto()
+    desconto_total = total - desconto
+    print("_________________________________")
+    print(f"Desconto: R${desconto:.2f}")
+    print(f'\nTotal: R${desconto_total:.2f}')
+    print("_________________________________")
+    mostrar_opcoes()
+
+def mostrar_opcoes():
+    print("Digite o número para selecionar a lista ordenada abaixo:")
+    print("1. Adicionar cupom de desconto")
+    print("2. Adicionar mais um item à lista")
+    print("3. Remover item da lista")
+    print("4. Prosseguir com o pagamento.")
+    print("5. Cancelar a compra\n")
+    resposta = input("Ou Digite qualquer tecla para finalizar a compra.\n")
+
+    match resposta:
+        case '1':
+            adicionar_cupom()
+        case '2':
+            registrar_mercadorias()
+        case '3':
             pass
+            ## permite remover algum item da lista
+            ## aaaaaaaaaaaaaaaa
+        case '4':
+            metodo_de_pagamento()
+        case '5':
+            print("Compra cancelada.")
+            pass
+            ## irá cancelar a compra
         case _:
-            ativo = False
+            metodo_de_pagamento()
 
-for produto, quantidade, preco in zip(produtos, quantidades, precos):
-    print(f"{produto} [{quantidade} unidade(s)]: R$ {preco:.8f}")
-total = 0
-for preco in precos:
-    total += preco
-print("_________________________________")
-print(f'\nTotal: R${total:.2f}')
-print("_________________________________")
+def adicionar_cupom():
+    cupom = input("Adicione o seu cupom:\n")
+    total = sum(precos)
 
-print("Digite o número para selecionar a lista ordenada abaixo:")
-print("1. Adicionar cupom de desconto")
-print("2. Remover item da lista")
-print("3. Cancelar a compra\n")
-resposta = input("Ou Digite qualquer tecla para finalizar a compra.\n")
+    if cupom in cupons_disponiveis and cupom not in cupons_ativados:
+        desconto = cupons_disponiveis[cupom]
+        if desconto >= total:
+            print(f"O cupom {cupom} não pode ser aplicado. O desconto excede o valor da compra.")
+        else:
+            cupons_ativados.append(cupom)
+            print(f"O cupom {cupom} foi adicionado! Desconto de R${desconto:.2f}.")
+    else:
+        print(f"O cupom {cupom} é inválido ou já foi usado.")
+    mostrar_lista()
 
-
-match resposta:
-    case '1':
-        cupom = input("Adicione o seu cupom")
-    case '2':
-        pass
-        ## permite remover algum item da lista
-        ## aaaaaaaaaaaaaaaa
-    case '3':
-        pass
-        ## irá cancelar a compra
-    case _:
-        pass
-        ## ao teclar qualquer tecla, finaliza a compra e seleciona o metodo de pagamento.
+if __name__ == "__main__":
+    registrar_mercadorias()
